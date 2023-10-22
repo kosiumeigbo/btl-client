b02f42dbbe743b27962989b098078d18274479ab
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-10-22 21:53:50 +0100

    create config.ts file to hold specific variables

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const NY_TIMES_API_KEY: string = "LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs";
+export const NY_TIMES_BEST_SELLERS_URL: string = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
+
+// NYTimes URL example: "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs"
+// OpenLibrary URL example: "https://openlibrary.org/api/books?bibkeys=ISBN:9781649374042&format=json&jscmd=data"
