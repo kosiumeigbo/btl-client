66ff6422054dd868ef055e97dde1052e246c34d4
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-10-22 22:47:07 +0100

    add functions to test out the NYTimes and OpenLibrary API's

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,21 @@
-import OuterTestComponent from "./components/OuterTestComponent.ts";
+// import HomePage from "./components/HomePage.ts";
+import { getNyTimesBestSellers } from "./model.ts";
 
-const root = document.querySelector("#root");
-const data: string = "TESTING!";
+const showNyTimesData = async function (): Promise<void> {
+  try {
+    const newData = await getNyTimesBestSellers();
+    if (!(newData instanceof Error)) console.log("NYTimes:", newData);
+  } catch (e) {
+    console.log(e);
+  }
+};
+showNyTimesData();
 
-root?.insertAdjacentElement("afterbegin", new OuterTestComponent(data));
+fetch("https://openlibrary.org/api/books?bibkeys=ISBN:9781649374042&format=json&jscmd=data")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("OpenLibrary:", data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
