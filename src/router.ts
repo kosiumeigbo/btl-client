3c8fa7940790a5721abbb85b382dab62d45f060c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-10-23 22:01:11 +0100

    update router.ts to test imported async functions

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,10 @@
 // import HomePage from "./components/HomePage.ts";
-import { getNyTimesBestSellers } from "./model.ts";
+import { getNyTimesBestSellers, getResultFromSearch } from "./model.ts";
 
-const showNyTimesData = async function (): Promise<void> {
-  try {
-    const newData = await getNyTimesBestSellers();
-    if (!(newData instanceof Error)) console.log("NYTimes:", newData);
-  } catch (e) {
-    console.log(e);
-  }
-};
-showNyTimesData();
+getNyTimesBestSellers().catch((e: Error) => {
+  console.log(e);
+});
 
-fetch("https://openlibrary.org/api/books?bibkeys=ISBN:9781649374042&format=json&jscmd=data")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("OpenLibrary:", data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+getResultFromSearch("9781649374042678").catch((e: Error) => {
+  console.log(e);
+});
