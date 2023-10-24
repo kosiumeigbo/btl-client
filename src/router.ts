7c84133c75e9192acb0de3bbbbffa54c9aec6fa6
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-10-24 08:34:02 +0100

    change function name to 'getBookObjFromOpenLibrary'

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 // import HomePage from "./components/HomePage.ts";
-import { getNyTimesBestSellers, getResultFromSearch } from "./model.ts";
+import { getNyTimesBestSellers, getBookObjFromOpenLibrary } from "./model.ts";
 
 getNyTimesBestSellers().catch((e: Error) => {
   console.log(e);
 });
 
-getResultFromSearch("9781649374042678").catch((e: Error) => {
+getBookObjFromOpenLibrary("9781649374042678").catch((e: Error) => {
   console.log(e);
 });
