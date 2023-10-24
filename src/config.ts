9bec76fd28c43c83f12eab1304060522598fb62c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-10-24 20:08:06 +0100

    create variable to hold time interval for calling the NYTimes API

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const NY_TIMES_API_KEY: string = "LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs";
 export const NY_TIMES_BEST_SELLERS_URL: string = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
+export const NY_TIMES_API_CALL_LIMIT_SECONDS: number = 240000;
 
 // NYTimes URL example: "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs"
 // OpenLibrary URL example: "https://openlibrary.org/api/books?bibkeys=ISBN:9781649374042&format=json&jscmd=data"
