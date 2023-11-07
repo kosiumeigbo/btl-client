4048f669816f497462de53f17dbb47e6f3aac8a9
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-11-07 22:32:16 +0000

    import hamburgerControl to router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 // import HomePage from "./components/HomePage.ts";
 import "./components/home-page";
 import { getBookObjFromOpenLibrary } from "./model";
+import "./hamburgerControl";
 
 getBookObjFromOpenLibrary("111").catch((e: Error) => {
   console.log(e);
