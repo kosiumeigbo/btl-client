a7af92f5478b6761737a6e2d4eda43c449e95452
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-11-09 18:20:03 +0000

    add in a new ISBN in the getBookObjFromOpenLibrary function

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,6 @@ import "./components/home-page";
 import { getBookObjFromOpenLibrary } from "./model";
 import "./hamburgerControl";
 
-getBookObjFromOpenLibrary("111").catch((e: Error) => {
+getBookObjFromOpenLibrary("978-0593669891").catch((e: Error) => {
   console.log(e);
 });
