4c09c027704bb8182dd1e52c15477ff2822bc2c1
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-11-22 21:12:09 +0000

    add Google Books API key

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const NY_TIMES_API_KEY: string = "LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs";
 export const NY_TIMES_BEST_SELLERS_URL: string = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
+export const GOOGLE_BOOKS_API_KEY: string = "AIzaSyAgq8935QbNIHZ27D5kf-LIWkUazKv8pV8";
 export const NY_TIMES_API_CALL_LIMIT_SECONDS: number = 240000;
 
 // NYTimes URL example: "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=LdtHYiyVFZ8XYsUSqTxpeal9VFqsvDbs"
