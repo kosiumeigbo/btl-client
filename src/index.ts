f15fb46a30dca13e219f32dbd0801d49b85c04bf
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-11-24 23:23:37 +0000

    test out Google Books API Endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import "./hamburgerControl";
 import "./components/search-page";
+import { GOOGLE_BOOKS_API_KEY } from "./config";
 import { getBookObjFromOpenLibrary } from "./model";
+
+fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:9780008247690&key=${GOOGLE_BOOKS_API_KEY}`)
+  .then((res) => res.json())
+  .then((data) => {
+    const [book] = data.items;
+    console.log(book);
+  });
 
 
 getBookObjFromOpenLibrary("978-0349437019").catch((e: Error) => {
