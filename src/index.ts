affe553f3ef4b3a29342e9071259872920d6044b
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-12-03 11:36:02 +0000

    update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import "./hamburgerControl";
 import "./components/search-page";
-import { GOOGLE_BOOKS_API_KEY } from "./config";
-import { getBookObjFromOpenLibrary } from "./model";
+import { getBookObj } from "./model";
 
-fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:9780008247690&key=${GOOGLE_BOOKS_API_KEY}`)
-  .then((res) => res.json())
-  .then((data) => {
-    const [book] = data.items;
-    console.log(book);
-  });
-
-
-getBookObjFromOpenLibrary("978-0349437019").catch((e: Error) => {
+getBookObj("9780349437019").catch((e: Error) => {
   console.log(e);
 });
