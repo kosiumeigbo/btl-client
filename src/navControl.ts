4b3a83159516d93362789795d172a20daff0147d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-12-04 21:29:41 +0000

    add logic for updating library total number in nav section

--- src/navControl.ts
+++ src/navControl.ts
@@ -3,8 +3,9 @@ import type { LibButtonPressedEventDetails } from "./types";
 const elBooksToRead = document.getElementById("books-to-read-total");
 const elBooksInProgress = document.getElementById("books-in-progress-total");
 const elBooksDone = document.getElementById("books-done-total");
+const elLibraryTotal = document.getElementById("library-total");
 
-if (elBooksToRead !== null && elBooksInProgress !== null && elBooksDone !== null) {
+if (elBooksToRead !== null && elBooksInProgress !== null && elBooksDone !== null && elLibraryTotal !== null) {
   document.addEventListener("lib-btn-pressed", (e) => {
     const detail = (e as CustomEvent<LibButtonPressedEventDetails>).detail;
     console.log(detail);
@@ -12,5 +13,10 @@ if (elBooksToRead !== null && elBooksInProgress !== null && elBooksDone !== null
     elBooksToRead.textContent = detail.totalBooksToRead().toString();
     elBooksInProgress.textContent = detail.totalBooksInProgress().toString();
     elBooksDone.textContent = detail.totalBooksDone().toString();
+    elLibraryTotal.textContent = (
+      detail.totalBooksToRead() +
+      detail.totalBooksInProgress() +
+      detail.totalBooksDone()
+    ).toString();
   });
 }
