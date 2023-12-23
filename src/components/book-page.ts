09ac45bc07ec3282c4eba764ba42ff7045de9333
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-12-23 16:45:39 +0100

    add btnPressed function for when an add-to-library button is pressed

--- src/components/book-page.ts
+++ src/components/book-page.ts
@@ -9,6 +9,35 @@ export default class BookPage extends HTMLElement {
 
   connectedCallback(): void {
     this.render();
+  btnPressed(e: Event): void {
+    const libButton = (e.target as HTMLElement).closest("[data-library][data-isbn]");
+    console.log(libButton);
+
+    if (libButton !== null) {
+      const isbn = (libButton as HTMLElement).dataset.isbn;
+      const btnLibrary = (libButton as HTMLElement).dataset.library;
+      console.log(isbn, btnLibrary);
+
+      if (typeof isbn === "string" && typeof btnLibrary === "string") {
+        const updatedCardState = addToLibraryBtnIsPressed(btnLibrary as LibraryLocation, isbn);
+        console.log(updatedCardState);
+
+        if (!(updatedCardState instanceof Error)) {
+          this.data = updatedCardState;
+        }
+      }
+
+      const libBtnPressed = new CustomEvent<LibButtonPressedEventDetails>("lib-btn-pressed", {
+        bubbles: true,
+        detail: {
+          totalBooksDone: () => state.libraryBooks.filter((book) => book.location === "booksDone").length,
+          totalBooksInProgress: () => state.libraryBooks.filter((book) => book.location === "booksInProgress").length,
+          totalBooksToRead: () => state.libraryBooks.filter((book) => book.location === "booksToRead").length
+        }
+      });
+      this.dispatchEvent(libBtnPressed);
+    }
+  }
   }
 
   render(): string {
